7fc329a607bcec48d9fddd169bf826de5758558e
wk633 <wk633@outlook.com>
2017-07-25 10:16:08 -0700

    add hot refresh middleware

--- server/app.js
+++ server/app.js
@@ -22,6 +22,9 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// hot refresh
+app.use(require('connect-livereload')());
+
 app.use('/', index);
 app.use('/users', users);
 
